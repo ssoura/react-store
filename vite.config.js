2185ebdb7dc7acebfcb4e31b274107d36b387e04
Sourabh Shukla <34943727+ssoura@users.noreply.github.com>
2023-01-05 20:54:39 +0530

    config change

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://dead-tan-anemone-sari.cyclic.app:5000",
         changeOrigin: true,
         secure: false,
       },
