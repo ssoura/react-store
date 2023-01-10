02af53b999d4b221df3882885c8d2de08d16a079
Sourabh Shukla <34943727+ssoura@users.noreply.github.com>
2023-01-11 04:52:37 +0530

    add env vars for client

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 3000,
-    proxy: {
-      "/api": {
-        // target: "https://dead-tan-anemone-sari.cyclic.app:5000",
-        target: "http://localhost:5000",
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react()],
+    server: {
+      host: true,
+      proxy: {
+        "/api": {
+          target: env.VITE_APP_API,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  };
 });
