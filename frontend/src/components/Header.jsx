5bd35637728f3fd8c5df8a20d2da6e0b61a1a336
Sourabh Shukla <34943727+ssoura@users.noreply.github.com>
2023-02-17 01:35:44 +0530

    fix build

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,5 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { LinkContainer } from "react-router-bootstrap";
-import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import SearchBox from "./SearchBox";
 import { logout } from "../store/slices/userSlice";
 
@@ -17,61 +14,70 @@ const Header = () => {
   };
 
   return (
-    <header>
-      <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
-        <Container>
-          <LinkContainer to="/">
-            <Navbar.Brand>shopping</Navbar.Brand>
-          </LinkContainer>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse
-            id="basic-navbar-nav"
-            className="justify-content-end"
-          >
-            <Routes>
-              <Route
-                render={({ history }) => <SearchBox history={history} />}
-              />
-            </Routes>
-            <Nav className="ml-auto">
-              <LinkContainer to="/cart">
-                <Nav.Link>
-                  <i className="fas fa-shopping-cart"></i> Cart
-                </Nav.Link>
-              </LinkContainer>
-              {userInfo ? (
-                <NavDropdown title={userInfo.name} id="username">
-                  <LinkContainer to="/profile">
-                    <NavDropdown.Item>Profile</NavDropdown.Item>
-                  </LinkContainer>
-                  <NavDropdown.Item onClick={logoutHandler}>
-                    Logout
-                  </NavDropdown.Item>
-                </NavDropdown>
-              ) : (
-                <LinkContainer to="/login">
-                  <Nav.Link>
-                    <i className="fas fa-user"></i> Sign In
-                  </Nav.Link>
-                </LinkContainer>
-              )}
-              {userInfo && userInfo.isAdmin && (
-                <NavDropdown title="Admin" id="adminmenu">
-                  <LinkContainer to="/admin/userlist">
-                    <NavDropdown.Item>Users</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to="/admin/productlist">
-                    <NavDropdown.Item>Products</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to="/admin/orderlist">
-                    <NavDropdown.Item>Orders</NavDropdown.Item>
-                  </LinkContainer>
-                </NavDropdown>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+    <header
+      className={`bg-gradient-to-r from-blue-700 to-indigo-900 transform shadow-xl sticky top-0 z-10 animate-dropIn`}
+    >
+      <div className="max-w-6xl mx-auto p-3 flex items-center space-x-4">
+        <h1 className="text-white w-10">
+          <Link to="/">shopping</Link>
+        </h1>
+        <div className="flex-1">
+          <Routes>
+            <Route render={({ history }) => <SearchBox history={history} />} />
+          </Routes>
+        </div>
+        <div className="flex space-x-4 hidden sm:block">
+          <Link to="/cart">
+            <span className="text-white">
+              <i className="fas fa-shopping-cart"></i> Cart
+            </span>
+          </Link>
+          {userInfo ? (
+            <>
+              <div className="dropdown dropdown-end">
+                <label tabIndex={0} className="text-white m-1">
+                  {userInfo.name}
+                </label>
+                <ul
+                  tabIndex={0}
+                  className="dropdown-content menu p-2 shadow bg-white rounded-box w-52"
+                >
+                  <li>
+                    <a className="block px-4 py-2">Item 1</a>
+                  </li>
+                  <li>
+                    <Link className="block px-4 py-2" to="/profile">
+                      Profile
+                    </Link>
+                  </li>
+                  <li>
+                    <a
+                      onClick={logoutHandler}
+                      href="#"
+                      className="block px-4 py-2"
+                    >
+                      Sign out
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </>
+          ) : (
+            <Link to="/login">
+              <span className="text-white">
+                <i className="fas fa-user"></i> Sign In
+              </span>
+            </Link>
+          )}
+          {userInfo && userInfo.isAdmin && (
+            <>
+              <Link to="/admin/userlist">Users</Link>
+              <Link to="/admin/productlist">Products</Link>
+              <Link to="/admin/orderlist">Orders</Link>
+            </>
+          )}
+        </div>
+      </div>
     </header>
   );
 };
