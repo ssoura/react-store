fa25dfe7b75b46cf56f465d95fcbe315fa96a9ec
= <=>
2025-01-23 00:49:15 +0530

    add dockerfile

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,5 @@
 export const BASE_URL =
-  process.env.NODE_ENV === 'develeopment' ? 'http://localhost:4000' : 'https://react-shop-wqg0.onrender.com';
+  process.env.NODE_ENV === 'develeopment' ? 'http://localhost:4000' : process.env.API_URL;
 export const PRODUCTS_URL = BASE_URL+'/api/products';
 export const USERS_URL = BASE_URL+'/api/users';
 export const ORDERS_URL = BASE_URL+'/api/orders';
